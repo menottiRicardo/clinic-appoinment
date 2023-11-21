71624976784fea853294780bad921db78d0c484f
ricardomenotti <ricardomenotti31@gmail.com>
2023-11-21 11:42:04 -0500

    auth microservice and moongose setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './core/guards/auth.guard';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        name: 'AUTH_SERVICE',
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            url: configService.get('RABBITMQ_URL'),
+            queue: configService.get('RABBITMQ_AUTH_QUEUE'),
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+        dbName: configService.get<string>('MONGO_DB_NAME'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_GUARD, useClass: JwtAuthGuard }],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+
+function configureSwagger(app): void {
+  const config = new DocumentBuilder()
+    .setTitle('appointmet-service')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/appt/api/docs', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.setGlobalPrefix('appt');
+
+  configureSwagger(app);
+
+  await app.startAllMicroservices();
+  await app.listen(configService.get('PORT'));
+  console.log(
+    '🚀 APPOINTMENT Service running on port: ' + configService.get('PORT'),
+  );
 }
 bootstrap();
