91cf33a6a9a1f27091f11b0321a2185faee97a83
ricardomenotti <ricardomenotti31@gmail.com>
2023-12-11 19:04:40 -0500

    health

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './core/decorators/allow.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Public()
+  @Get('health')
   status(): string {
-    console.log('hre>>2 from app.controller.ts')
+    console.log('hre>>2 from app.controller.ts');
     return this.appService.getHello();
   }
 }
